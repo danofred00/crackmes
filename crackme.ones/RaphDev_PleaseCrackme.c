a31b352b7ad08f3cfeb92dddf08fb3e240f9b5c7
Daniel Leussa <67153034+danofred00@users.noreply.github.com>
2022-09-29 21:58:28 +0100

    Add files via upload

---
+++ crackme.ones/RaphDev_PleaseCrackme.c
@@ -0,0 +1,48 @@
+/*
+	Powered by crackmes.one
+	This is a keygen for RaphDev's Crackme called PleaseCackme
+	@author Danofred
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+// the buffer's size for username, entry_password and 
+// great_password is 32 bytes
+
+#define STR_LEN 32
+
+int main(int argc, char const *argv[])
+{
+
+	char username[STR_LEN];
+	char password[STR_LEN];
+	char greatPassword[STR_LEN];
+	int num_entry;
+
+	// getting username
+	printf("%s\n", "Enter your username :");
+	scanf("%s", username);
+
+
+	printf("%s\n", "Choose any number between 0 and 9 :");
+	scanf("%d", &num_entry);
+
+	if (num_entry < 0)
+	{
+		printf("%s\n", "Number is too small");
+	} else if (num_entry > 9) {
+		printf("%s\n", "Number is too big");
+	} else {
+
+		// here is generatePassword method
+		for (int i = 0; i < strlen(username); ++i)
+		{
+			greatPassword[i] = username[i] + num_entry;
+		}
+
+		printf("Your password is : %s\n", greatPassword);
+	}
+
+	return 0;
+}
